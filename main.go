098761013588815ff98f85cf5f84bb4ae6ef760f
Harshit Pratap Singh <harshit.singh@ima360.com>
2021-11-03 09:15:14 +0530

    added comments

--- main.go
+++ main.go
@@ -27,25 +27,31 @@ func main() {
 	fmt.Println("Stage Name-\t ", os.Getenv("GO_STAGE_NAME"))
 	fmt.Println("Job Name-\t ", os.Getenv("GO_JOB_NAME"))
 
-	//Custome Var using go program
+	//Custom Var using go program
 	os.Setenv("NotValid", "New Var Through Go")
+
 	//Checking Existing ones
 	CheckEnvironmentVariable("SampleString")
 	CheckEnvironmentVariable("SampleString2")
 	CheckEnvironmentVariable("NotValid")
 
+	//Default router initialization
 	router := gin.Default()
 
+	//Endpoint to access our environment variables by Passing a name.
 	router.GET("/var/:stringVar", func(c *gin.Context) {
-
+		//Returning result
 		c.JSON(200, gin.H{
 			"result": CheckEnvironmentVariable(c.Param("stringVar")),
 		})
 
 	})
 
+	//Close the server when needed
 	router.GET("/closeServer", func(c *gin.Context) {
+		//deferring the stop of container
 		defer exec.Command("docker", "stop", "helloworldenv")
+		//logging and closing the gin server by simply interrupting the program execution.
 		log.Fatal("Exiting the Server")
 	})
 
